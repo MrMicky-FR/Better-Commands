866f382b19b2d4f91c44b8345bc227373b8a655c
MrMicky <git@mrmicky.fr>
2019-01-11 15:45:27 +0100

    Small cleanup

--- src/main/java/fr/mrmicky/bettercommands/CommandManager.java
+++ src/main/java/fr/mrmicky/bettercommands/CommandManager.java
@@ -7,7 +7,6 @@ import org.bukkit.command.SimpleCommandMap;
 import org.bukkit.plugin.Plugin;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -15,7 +14,6 @@ import java.util.Set;
 public class CommandManager {
 
     private static final CommandMap BUKKIT_COMMAND_MAP;
-    private static final Method REGISTER_COMMAND_METHOD;
     private static final Field KNOWN_COMMANDS_FIELD;
 
     private static String noPermissionMessage = ChatColor.RED + "You don't have the permission to perform this command!";
@@ -28,23 +26,14 @@ public class CommandManager {
     }
 
     static {
-        Object commandMap = null;
-        Method registerCommandMethod = null;
-        Field knownCommandField = null;
-
         try {
-            commandMap = Bukkit.getServer().getClass().getDeclaredMethod("getCommandMap").invoke(Bukkit.getServer());
+            BUKKIT_COMMAND_MAP = (CommandMap) Bukkit.getServer().getClass().getDeclaredMethod("getCommandMap").invoke(Bukkit.getServer());
 
-            registerCommandMethod = SimpleCommandMap.class.getDeclaredMethod("register", String.class, org.bukkit.command.Command.class);
-            knownCommandField = SimpleCommandMap.class.getDeclaredField("knownCommands");
-            knownCommandField.setAccessible(true);
+            KNOWN_COMMANDS_FIELD = SimpleCommandMap.class.getDeclaredField("knownCommands");
+            KNOWN_COMMANDS_FIELD.setAccessible(true);
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
-
-        BUKKIT_COMMAND_MAP = (CommandMap) commandMap;
-        REGISTER_COMMAND_METHOD = registerCommandMethod;
-        KNOWN_COMMANDS_FIELD = knownCommandField;
     }
 
     public void registerCommand(Command command) {
@@ -52,12 +41,8 @@ public class CommandManager {
             return; // TODO do something ?
         }
 
-        try {
-            REGISTER_COMMAND_METHOD.invoke(BUKKIT_COMMAND_MAP, plugin.getName(), command.bukkitCommand);
-            commands.add(command);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        BUKKIT_COMMAND_MAP.register(plugin.getName(), command.bukkitCommand);
+        commands.add(command);
     }
 
     public void unregisterAll() {
@@ -69,9 +54,9 @@ public class CommandManager {
             return; // TODO do something ?
         }
 
-        try {
-            org.bukkit.command.Command bukkitCommand = command.bukkitCommand;
+        org.bukkit.command.Command bukkitCommand = command.bukkitCommand;
 
+        try {
             Map<?, ?> knownCommands = (Map<?, ?>) KNOWN_COMMANDS_FIELD.get(BUKKIT_COMMAND_MAP);
 
             knownCommands.values().removeIf(bukkitCommand::equals);
